db1361f488d04af1009b1f5b4f70414fc6a543f9
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-03-12 14:21:33 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vedadiyan/strseg
+
+go 1.21.3
---
+++ strseg.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func CreateParser[T any]() func(str string, sep string) (*T, error) {
+	value := new(T)
+	typeOfT := reflect.TypeOf(*value)
+	fieldLen := typeOfT.NumField()
+	return func(str string, sep string) (t *T, err error) {
+		value := new(T)
+		segments := strings.Split(str, sep)
+		valueOfT := reflect.ValueOf(value)
+		for i := 0; i < fieldLen; i++ {
+			field := typeOfT.Field(i)
+			tag := field.Tag.Get("index")
+			if len(tag) == 0 {
+				continue
+			}
+			if field.Type.Kind() != reflect.String {
+				return nil, fmt.Errorf("expected string but found %s", field.Type.Kind())
+			}
+			index, err := strconv.Atoi(tag)
+			if err != nil {
+				return nil, err
+			}
+			if index >= len(segments) {
+				return nil, fmt.Errorf("index out of range")
+			}
+			valueOfT.Elem().Field(i).Set(reflect.ValueOf(segments[index]))
+
+		}
+		return value, nil
+	}
+}
